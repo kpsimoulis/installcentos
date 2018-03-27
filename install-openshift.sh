5b16a2d262a770e009b3dc5f2f281127e0c9c278
Konstantinos Psimoulis <kosta@noima.com>
2018-03-27 15:48:47 -0400

    updating repo to match the changes

--- install-openshift.sh
+++ install-openshift.sh
@@ -7,7 +7,7 @@ export USERNAME=${USERNAME:="$(whoami)"}
 export PASSWORD=${PASSWORD:=password}
 export VERSION=${VERSION:="v3.7.1"}
 
-export SCRIPT_REPO=${SCRIPT_REPO:="https://raw.githubusercontent.com/gshipley/installcentos/master"}
+export SCRIPT_REPO=${SCRIPT_REPO:="https://raw.githubusercontent.com/kpsimoulis/installcentos/master"}
 
 export IP="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"
 export API_PORT=${API_PORT:="8443"}
